6f7a624997c9fb27b0b30bd85a51997ee647fd4c
AurelienNioche <nioche.aurelien@gmail.com>
2016-12-15 18:10:19 +0100

    Modify parameters for both mains.

--- RL_main.py
+++ RL_main.py
@@ -9,10 +9,10 @@ from analysis import represent_results
 if __name__ == "__main__":
 
     parameters = {
-        "t_max": 200,
+        "t_max": 500,
         "agent_parameters": {"alpha": 0.1, "temp": 0.01},
-        "role_repartition": np.array([100, 100, 100]),
-        "storing_costs": np.array([0.1, 0.25, 0.265]),
+        "role_repartition": np.array([500, 500, 500]),
+        "storing_costs": np.array([0.1, 0.25, 0.5]),
         "kw_model": ModelA,
         "agent_model": RLAgent,
     }
@@ -22,4 +22,4 @@ if __name__ == "__main__":
             **parameters
         )
 
-    represent_results(backup=backup, parameters=parameters, fig_name=path.expanduser("~/Desktop/KW.pdf"))
+    represent_results(backup=backup, parameters=parameters, fig_name=path.expanduser("~/Desktop/KW_RL_Agents.pdf"))
--- duffy_main.py
+++ duffy_main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     parameters = {
         "t_max": 500,
         "agent_parameters": {"beta": 0.9, "u": 1},
-        "role_repartition": np.array([200, 200, 200]),
+        "role_repartition": np.array([500, 500, 500]),
         "storing_costs": np.array([0.1, 0.25, 0.5]),
         "kw_model": ModelA,
         "agent_model": DuffyAgent,
@@ -22,4 +22,4 @@ if __name__ == "__main__":
             **parameters
         )
 
-    represent_results(backup=backup, parameters=parameters, fig_name=path.expanduser("~/Desktop/KW.pdf"))
+    represent_results(backup=backup, parameters=parameters, fig_name=path.expanduser("~/Desktop/KW_Duffy_Agents.pdf"))
